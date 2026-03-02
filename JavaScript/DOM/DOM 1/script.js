1e98717af4e1f1238f793cfc31a977ceaa1eea65
matteopadesi-byte <matteo.padesi@allievi.itsdigitalacademy.com>
2026-03-02 17:57:19 +0100

    Esercizi DOM 02/03/2026
    Esercizi DOM.

---
+++ JavaScript/DOM/DOM 1/script.js
@@ -0,0 +1,8 @@
+const titolo = document.querySelector("#titolo");
+const paragrafi = document.querySelectorAll(".descrizione");
+const lista = document.querySelectorAll("li");
+const listo = document.parentElement("li");
+
+console.log(titolo.textContent);
+console.log(paragrafi.length);
+console.log(lista[1].textContent);
